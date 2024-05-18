6681506767f74a5e8b9218e535c09313095a12e1
yuyu-78 <160311234+yuyu-78@users.noreply.github.com>
2024-05-19 06:15:57 +0900

    課題007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $vegetables_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($vegetables_data);
+  ?>
+  </p>
+  
+</body>
+</html>
